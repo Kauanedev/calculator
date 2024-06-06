faeec5692c290ea2b3f793ad1fc9c3f1e480dd0b
Kauane Santos <kauanesantos.coding@gmail.com>
2024-06-06 16:06:36 -0300

    ♻️refactor: change input to button

--- src/Page/main.js
+++ src/Page/main.js
@@ -1,46 +1,51 @@
 import './main.css';
 
 function Main() {
+
+
   return (
     <div className="container">
+
       <div className="calculator">
         <div className="form">
           <div className="display">
             <input type="text" />
           </div>
           <div>
-            <input type="button" value="C" />
-            <input type="button" value="%" />
-            <input type="button" value="Del" />
-            <input type="button" value="/" />
+            <button value='C'>C</button>
+            <button >%</button>
+            <button>Del</button>
+            <button >/</button>
           </div>
           <div>
-            <input type="button" value={7} />
-            <input type="button" value={8} />
-            <input type="button" value={9} />
-            <input type="button" value="x" />
+
+            <button value={7}>7</button>
+            <button value={8}>8</button>
+            <button value={9}>9</button>
+            <button value='x'>x</button>
           </div>
           <div>
-            <input type="button" value={4} />
-            <input type="button" value={5} />
-            <input type="button" value={6} />
-            <input type="button" value="-" />
+            <button value={4}>4</button>
+            <button value={5}>5</button>
+            <button value={6}>6</button>
+            <button value='-'>-</button>
           </div>
           <div>
-            <input type="button" value={1} />
-            <input type="button" value={2} />
-            <input type="button" value={3} />
-            <input type="button" value="+" />
+            <button value={1}>1</button>
+            <button value={2}>2</button>
+            <button value={3}>3</button>
+            <button value='+'>+</button>
           </div>
           <div>
-            <input type="button" value="Pi" />
-            <input type="button" value={0} />
-            <input type="button" value={'.'} />
-            <input type="button" value="=" />
+            <button value='Pi'>Pi</button>
+            <button value={0}>0</button>
+            <button value='.'>.</button>
+            <button value='='>=</button>
           </div>
         </div>
-      </div>
-    </div>
+      </div >
+
+    </div >
   );
 }
 
