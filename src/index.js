22ce0300d5ba8d1d10a2c38095be84182e45fb87
Kauane Santos <kauanesantos.coding@gmail.com>
2024-06-06 14:40:26 -0300

    🔧chore: create Page folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './App';
+import Main from './Page/main';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
