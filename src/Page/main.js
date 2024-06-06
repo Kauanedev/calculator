1b8f128675a6b71b4912f1e22b583bcd9214b3e0
Kauane Santos <kauanesantos.coding@gmail.com>
2024-06-06 16:06:52 -0300

    ✨feat: create functions

--- src/Page/main.js
+++ src/Page/main.js
@@ -1,46 +1,88 @@
+import {useState} from 'react';
 import './main.css';
 
 function Main() {
 
+  const [value, setValue] = useState(0)
+  const [previousNum, setPreiousNum] = useState(0)
+  const [operator, setOperator] = useState('')
 
+  function inputNum(e) {
+    const input = e.target.value;
+    if (value === 0) {
+      setValue(input)
+    }
+    else {
+      setValue(value + input)
+    }
+  }
+
+  function deleteLast() {
+    const newValue = value.toString()
+
+    setValue(newValue.slice(0, newValue.length - 1))
+  }
+
+  function operatorHandler(e) {
+    const operatorInput = e.target.value;
+    setOperator(operatorInput);
+    setPreiousNum(value);
+    setValue(0);
+  }
+
+  function percentage() {
+    setValue(value / 100);
+  }
+
+  function calculate() {
+    if (operator === "/") {
+      setValue(parseFloat(previousNum) / parseFloat(value));
+    } else if (operator === "x") {
+      setValue(parseFloat(previousNum) * parseFloat(value));
+    } else if (operator === "-") {
+      setValue(parseFloat(previousNum) - parseFloat(value));
+    } else if (operator === "+") {
+      setValue(parseFloat(previousNum) + parseFloat(value));
+    }
+  }
   return (
     <div className="container">
 
       <div className="calculator">
         <div className="form">
           <div className="display">
-            <input type="text" />
+            <input type="text" value={value} />
           </div>
           <div>
-            <button value='C'>C</button>
-            <button >%</button>
-            <button>Del</button>
-            <button >/</button>
+            <button onClick={e => setValue('')} value='C'>C</button>
+            <button onClick={percentage}>%</button>
+            <button onClick={deleteLast}>Del</button>
+            <button onClick={operatorHandler}>/</button>
           </div>
           <div>
 
-            <button value={7}>7</button>
-            <button value={8}>8</button>
-            <button value={9}>9</button>
-            <button value='x'>x</button>
+            <button onClick={inputNum} value={7}>7</button>
+            <button onClick={inputNum} value={8}>8</button>
+            <button onClick={inputNum} value={9}>9</button>
+            <button onClick={operatorHandler} value='x'>x</button>
           </div>
           <div>
-            <button value={4}>4</button>
-            <button value={5}>5</button>
-            <button value={6}>6</button>
-            <button value='-'>-</button>
+            <button onClick={inputNum} value={4}>4</button>
+            <button onClick={inputNum} value={5}>5</button>
+            <button onClick={inputNum} value={6}>6</button>
+            <button onClick={operatorHandler} value='-'>-</button>
           </div>
           <div>
-            <button value={1}>1</button>
-            <button value={2}>2</button>
-            <button value={3}>3</button>
-            <button value='+'>+</button>
+            <button onClick={inputNum} value={1}>1</button>
+            <button onClick={inputNum} value={2}>2</button>
+            <button onClick={inputNum} value={3}>3</button>
+            <button onClick={operatorHandler} value='+'>+</button>
           </div>
           <div>
-            <button value='Pi'>Pi</button>
-            <button value={0}>0</button>
-            <button value='.'>.</button>
-            <button value='='>=</button>
+            <button onClick={e => setValue(3.14159265358979)} value='Pi'>Pi</button>
+            <button onClick={inputNum} value={0}>0</button>
+            <button onClick={inputNum} value='.'>.</button>
+            <button onClick={calculate} value='='>=</button>
           </div>
         </div>
       </div >
